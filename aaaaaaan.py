8ac09590ed162893afac7d61fa299f29ccf56e8e
Shachikou <166305346+Shachikou@users.noreply.github.com>
2024-08-20 22:42:18 +0900

    Update check.py

--- aaaaaaan.py
+++ aaaaaaan.py
@@ -6,13 +6,9 @@ while True:
     username = input("ユーザー名")
     time.sleep(1)
     url = "https://scratch.mit.edu/users/"+username+"/"
-    #print(url)
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
-    #title = soup.find("h1").text
-    #print(html)
-    #print('404'in html)
     if '404'in html:
             print("存在しません")
             print("/")
